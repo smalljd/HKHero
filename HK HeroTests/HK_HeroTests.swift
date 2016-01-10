ccd542e27fb83abf8896abd4643873e7a0fcbc74
Small, Jeff <Jeff.Small@capitalone.com>
2016-01-10 13:53:27 -0800

    Initial Commit

---
+++ HK HeroTests/HK_HeroTests.swift
@@ -0,0 +1,36 @@
+//
+//  HK_HeroTests.swift
+//  HK HeroTests
+//
+//  Created by Small, Jeff on 1/10/16.
+//  Copyright © 2016 Small, Jeff. All rights reserved.
+//
+
+import XCTest
+@testable import HK_Hero
+
+class HK_HeroTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
