1099fe4e38d5097d73cea53b7499280b50767965
Small, Jeff <Jeff.Small@capitalone.com>
2016-01-13 15:23:43 -0800

    Initial large commit

---
+++ HK Hero WatchKit Extension/WorkoutTypeController.swift
@@ -0,0 +1,31 @@
+//
+//  WorkoutTypeController.swift
+//  HK Hero
+//
+//  Created by Small, Jeff on 1/10/16.
+//  Copyright © 2016 Small, Jeff. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class WorkoutTypeController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ HK Hero/HealthStore.swift
@@ -0,0 +1,9 @@
+//
+//  HealthStore.swift
+//  HK Hero
+//
+//  Created by Small, Jeff on 1/10/16.
+//  Copyright © 2016 Small, Jeff. All rights reserved.
+//
+
+import Foundation
